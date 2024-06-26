7549417ed4feb102591b83582dd3822f9e20395b
Orban Tamas <orbant1@gmail.com>
2024-06-26 17:44:23 +0200

    chat Websocket

--- client/client/app/utils/assist/messageStateChanger.js
+++
@@ -1,6 +0,0 @@
-export const messageStateChange = async (chatState) => {
-    const updatedChatState = chatState.map((msg, index) => 
-        index === chatState.length - 1 ? { ...msg, sent: true } : msg
-    );
-    return updatedChatState
-}
