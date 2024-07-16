4264de459d2142e0b5a6dd864419c9f3b6096ba5
Tamas Orban <124793231+orbant12@users.noreply.github.com>
2024-07-16 09:42:33 +0200

    Delete client directory

--- client/app/Tests/home.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render, fireEvent, waitFor, act } from '@testing-library/react-native';
-import TabOneScreen from '../pages/Home/home';
-import { useAuth } from '../context/UserAuthContext';
-import { fetchMonthTasks, fetchReminders, fetchOutDatedSpots, fetchRiskySpots, fetchUnfinishedSpots, fetchUserData } from '../services/server';
-
- 
